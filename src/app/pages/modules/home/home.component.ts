2a8fb5175bf248d831a45abc571f0cdd145fff9b
wanly13 <wanlyobregon13@gmail.com>
2024-01-30 20:42:36 -0500

    agreanado

--- src/app/pages/modules/home/home.component.ts
+++ src/app/pages/modules/home/home.component.ts
@@ -1,13 +1,31 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { CarouselModule } from 'primeng/carousel';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, CarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  
+  responsiveOptions = [
+    {
+      breakpoint: '1199px',
+      numVisible: 1,
+      numScroll: 1
+    },
+    {
+      breakpoint: '991px',
+      numVisible: 2,
+      numScroll: 1
+    },
+    {
+      breakpoint: '767px',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ];
 }
