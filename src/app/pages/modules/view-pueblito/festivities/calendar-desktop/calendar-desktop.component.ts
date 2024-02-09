da3eb6ef28c79fdb5ae50721f06eafd5b1dcf3a9
wanly13 <wanlyobregon13@gmail.com>
2024-02-09 09:11:32 -0500

    error carrusel!

--- src/app/pages/modules/view-pueblito/festivities/calendar-desktop/calendar-desktop.component.ts
+++ src/app/pages/modules/view-pueblito/festivities/calendar-desktop/calendar-desktop.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { CarouselModule } from 'primeng/carousel';
 import moment from 'moment';
 import { CaruselComponent } from '../carusel/carusel.component';
+import { FestivitiesService } from '../../../../../services/festivities.service';
 
 @Component({
   selector: 'app-calendar-desktop',
@@ -21,37 +22,70 @@ export class CalendarDesktopComponent {
   dateSelect: any; // La fecha que se esta traendo los dias a mostrar
 
   week: any = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+  constructor(
+    private festivitiesService: FestivitiesService,
+  ) { }
 
 
   ngOnInit() {
+    /* const data = {
+      "mes": "2",
+      "anio": "2024",
+      "lugar": "97d1a344-6c48-422c-b790-e44f5a10497f"
+    }
+    console.log("sadf");
+*/
+    //this.getDataCalendar(data); 
     this.getDaysFromDate(this.mesElegido, this.anioElegido)
   }
 
-  getDaysFromDate(month: any, year: any) { // Trae todos los dias del mes
-    //console.log("fechas: ", month, year);
+  getDaysFromDate(month: any, year: any) {
+    this.monthSelect = []
+    const data = {
+      "mes": month.toString(),
+      "anio": year.toString(),
+      "lugar": "97d1a344-6c48-422c-b790-e44f5a10497f"
+    }
+    this.festivitiesService.search_events_dia(data).subscribe(
+      (response: any) => {
+        //this.functAgendaMes(response, month_selected)
+        const startDate = moment(`${year}-${month}-01`, 'YYYY-MM-DD')
+        const endDate = startDate.clone().endOf('month')
 
-    const startDate = moment(`${year}-${month}-01`, 'YYYY-MM-DD')
-    const endDate = startDate.clone().endOf('month')
+        const diffDays = endDate.diff(startDate, 'days', true)
+        const numberDays = Math.round(diffDays);
 
-    const diffDays = endDate.diff(startDate, 'days', true)
-    const numberDays = Math.round(diffDays);
-
-    const arrayDays = Object.keys([...Array(numberDays)]).map((a: any) => {
-      a = parseInt(a) + 1;
-      const dayObject = moment(`${year}-${month}-${a}`, 'YYYY-MM-DD');
-      return {
-        name: dayObject.format("dddd"),
-        value: a,
-        indexWeek: dayObject.isoWeekday(),
-        fecha: dayObject.format(),
-        class: 'bg-blue-400'
-      };
-    });
-    this.dateSelect = startDate;
-    this.monthSelect = arrayDays;
-    //console.log("mes seleccionado", this.monthSelect);
-    this.functAgendaMes()
+        const arrayDays = Object.keys([...Array(numberDays)]).map((a: any) => {
+          a = parseInt(a) + 1;
+          const dayObject = moment(`${year}-${month}-${a}`, 'YYYY-MM-DD');
+          return {
+            name: dayObject.format("dddd"),
+            value: a,
+            indexWeek: dayObject.isoWeekday(),
+            fecha: dayObject.format(),
+            class: 'bg-blue-400',
+            events: []
+          };
+        });
+        this.dateSelect = startDate;
+        //this.monthSelect = arrayDays;
+        this.functAgendaMes(response, arrayDays)
+      },
+      err => {
+      }
+    )
   }
+  //days_month: any[] = []
+  getDataCalendar(data: any, month_selected: any) {
+    this.festivitiesService.search_events_dia(data).subscribe(
+      (response: any) => {
+        this.functAgendaMes(response, month_selected)
+
+      },
+      err => {
+      }
+    )
+  };
 
   changeMonth(flag: any) {
     if (flag < 0) {
@@ -67,11 +101,6 @@ export class CalendarDesktopComponent {
       this.getDaysFromDate(nextDate.format("MM"), nextDate.format("YYYY"));
     }
   }
-
-
-
-  diasAtendidos: any = "font-normal px-1 rounded h-8 text-sm 2xl:text-xl 2xl:h-14 mx-2 my-1 bg-green-300 border-green-500 flex justify-center items-center text-white  hover:border-green-500 hover:text-green-500 cursor-pointer";
-  diasValidos: any = "font-bold px-1 rounded h-8 text-sm 2xl:text-xl 2xl:h-14 mx-2 my-1 bg-green-300 border-green-500 flex justify-center items-center text-white  hover:border-green-500 hover:text-green-500 cursor-pointer";
 
 
   days_month = [
@@ -225,18 +254,14 @@ export class CalendarDesktopComponent {
 
   ]
 
-  functAgendaMes() {
-    const data = this.days_month;
+  functAgendaMes(days: any, month_selected: any) {
+    const data = days;
     const result: any[] = [];
 
-    this.monthSelect.forEach((item1) => {
+    month_selected.forEach((item1) => {
       const item2 = data.find((item2: any) => {
         let p1 = moment.utc(item2.fecha);
         let p2 = moment.utc(item1.fecha);
-        /* console.log("P1", p1);
-        console.log("P2", p2); */
-
-        // Comparar fechas con Moment.js
         return p1.isSame(p2, 'day');
       });
 
@@ -260,9 +285,38 @@ export class CalendarDesktopComponent {
         });
       }
     });
-
-    this.monthSelect = result;
+    setTimeout(() => {
+      this.monthSelect = result;
+    }, 1000)
+    
     //console.log("FINAL: ", this.monthSelect);
   }
+  responsiveOptions = [
 
+    {
+      breakpoint: '1536px',
+      numVisible: 1,
+      numScroll: 1
+    },
+    {
+      breakpoint: '1280px',
+      numVisible: 1,
+      numScroll: 1
+    },
+    {
+      breakpoint: '1024px',
+      numVisible: 1,
+      numScroll: 1
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 1,
+      numScroll: 1
+    },
+    {
+      breakpoint: '640px',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ];
 }
