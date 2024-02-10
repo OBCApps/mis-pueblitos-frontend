70653e9d1ea7e425b34921e40ebb7332c6fa0e04
AngelMoraH <angel.mora@utec.edu.pe>
2024-02-09 21:15:12 -0500

    subevento detail

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true
 };
 
-export const API_SERVER = "http://localhost:3000";
-//export const API_SERVER = 'https://mis-pueblitos-backend.onrender.com'
+//export const API_SERVER = "http://localhost:3000";
+export const API_SERVER = 'https://mis-pueblitos-backend.onrender.com'
