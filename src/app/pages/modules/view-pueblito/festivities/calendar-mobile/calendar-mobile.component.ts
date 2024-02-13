cff63597b7b68a940bee8e2eb0ec090cabb41406
wanly13 <wanlyobregon13@gmail.com>
2024-02-13 12:18:46 -0500

    cambio de fechas, en lista y mobile

--- src/app/pages/modules/view-pueblito/festivities/calendar-mobile/calendar-mobile.component.ts
+++ src/app/pages/modules/view-pueblito/festivities/calendar-mobile/calendar-mobile.component.ts
@@ -1,13 +1,18 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import moment from 'moment';
 import { CarouselModule } from 'primeng/carousel';
 import { CaruselComponent } from '../carusel/carusel.component';
+import { register } from 'swiper/element';
+import { Router } from '@angular/router';
+import { FestivitiesService } from '../../../../../services/festivities.service';
+register();
 
 @Component({
   selector: 'app-calendar-mobile',
   standalone: true,
   imports: [CarouselModule, NgFor, CaruselComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './calendar-mobile.component.html',
   styleUrl: './calendar-mobile.component.scss'
 })
@@ -21,239 +26,122 @@ export class CalendarMobileComponent {
   dateSelect: any; // La fecha que se esta traendo los dias a mostrar
 
   week: any = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
-
+  constructor(
+    private festivitiesService: FestivitiesService,
+    private router: Router
+  ) {}
   ngOnInit() {
     this.getDaysFromDate(this.mesElegido, this.anioElegido)
   }
+  goToSubEventoDetail(item: any) {
+    console.log('item: ', item);
+    const queryParamsObject = {
+      id_father: item.id_father,
+    };
 
-  getDaysFromDate(month: any, year: any) { // Trae todos los dias del mes
-    console.log("fechas: ", month, year);
-
-    const startDate = moment(`${year}-${month}-01`, 'YYYY-MM-DD')
-    const endDate = startDate.clone().endOf('month')
-
-    const diffDays = endDate.diff(startDate, 'days', true)
-    const numberDays = Math.round(diffDays);
-
-    const arrayDays = Object.keys([...Array(numberDays)]).map((a: any) => {
-      a = parseInt(a) + 1;
-      const dayObject = moment(`${year}-${month}-${a}`, 'YYYY-MM-DD');
-      return {
-        name: dayObject.format("dddd"),
-        value: a,
-        indexWeek: dayObject.isoWeekday(),
-        fecha: dayObject.format(),
-        class: 'bg-blue-400'
-      };
+    this.router.navigate(['pueblitos/subeventodetail'], {
+      queryParams: queryParamsObject,
     });
-    this.dateSelect = startDate;
-    this.monthSelect = arrayDays;
+  }
+  getDaysFromDate(month: any, year: any) {
+    //this.monthSelect = []
+    const data = {
+      mes: month.toString(),
+      anio: year.toString(),
+      lugar: '97d1a344-6c48-422c-b790-e44f5a10497f',
+    };
+    this.festivitiesService.search_events_dia(data).subscribe(
+      (response: any) => {
+        //this.functAgendaMes(response, month_selected)
+        const startDate = moment(`${year}-${month}-01`, 'YYYY-MM-DD');
+        const endDate = startDate.clone().endOf('month');
 
-    this.functAgendaMes()
+        const diffDays = endDate.diff(startDate, 'days', true);
+        const numberDays = Math.round(diffDays);
+
+        const arrayDays = Object.keys([...Array(numberDays)]).map((a: any) => {
+          a = parseInt(a) + 1;
+          const dayObject = moment(`${year}-${month}-${a}`, 'YYYY-MM-DD');
+          return {
+            name: dayObject.format('dddd'),
+            value: a,
+            indexWeek: dayObject.isoWeekday(),
+            fecha: dayObject.format(),
+            class: 'bg-blue-400',
+            events: [],
+          };
+        });
+        this.dateSelect = startDate;
+        //this.monthSelect = arrayDays;
+        this.functAgendaMes(response, arrayDays);
+      },
+      (err) => {}
+    );
+  }
+  //days_month: any[] = []
+  getDataCalendar(data: any, month_selected: any) {
+    this.festivitiesService.search_events_dia(data).subscribe(
+      (response: any) => {
+        this.functAgendaMes(response, month_selected);
+      },
+      (err) => {}
+    );
   }
 
   changeMonth(flag: any) {
     if (flag < 0) {
-      const prevDate = this.dateSelect.clone().subtract(1, "month");
-      this.anioElegido = Number(prevDate.format("YYYY")); this.mesElegido = Number(prevDate.format("MM"));
+      const prevDate = this.dateSelect.clone().subtract(1, 'month');
+      this.anioElegido = Number(prevDate.format('YYYY'));
+      this.mesElegido = Number(prevDate.format('MM'));
       console.log(this.mesElegido);
-      this.getDaysFromDate(prevDate.format("MM"), prevDate.format("YYYY"));
+      this.getDaysFromDate(prevDate.format('MM'), prevDate.format('YYYY'));
     } else {
-      const nextDate = this.dateSelect.clone().add(1, "month");
-      this.anioElegido = Number(nextDate.format("YYYY")); this.mesElegido = Number(nextDate.format("MM"));
+      const nextDate = this.dateSelect.clone().add(1, 'month');
+      this.anioElegido = Number(nextDate.format('YYYY'));
+      this.mesElegido = Number(nextDate.format('MM'));
       console.log(this.mesElegido);
 
-      this.getDaysFromDate(nextDate.format("MM"), nextDate.format("YYYY"));
+      this.getDaysFromDate(nextDate.format('MM'), nextDate.format('YYYY'));
     }
   }
 
-  days_month = [
-    {
-      fecha: '2024-02-01',
-      events: [
-        {
-          name_subevent: 'Dia Central Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-        {
-          name_subevent: 'Aniversario Amauta Atusparia',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-      ]
-    },
-    {
-      fecha: '2024-02-02',
-      events: [
-        {
-          name_subevent: 'Carrera de Cientas Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-        {
-          name_subevent: '1ra Tarde Taurina Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-      ]
-    },
-    {
-      fecha: '2024-02-03',
-      events: [
-        {
-          name_subevent: '2ra Tarde Taurina Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-        {
-          name_subevent: '3ra Tarde Taurina Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-      ]
-    },
-    {
-      fecha: '2024-02-14',
-      events: [
-        {
-          name_subevent: '2ra Tarde Taurina Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-        {
-          name_subevent: '3ra Tarde Taurina Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-      ]
-    },
-    {
-      fecha: '2024-02-15',
-      events: [
-        {
-          name_subevent: '2ra Tarde Taurina Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-        {
-          name_subevent: '3ra Tarde Taurina Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-      ]
-    },
-    {
-      fecha: '2024-02-28',
-      events: [
-        {
-          name_subevent: '2ra Tarde Taurina Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-        {
-          name_subevent: '3ra Tarde Taurina Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-      ]
-    },
-    {
-      fecha: '2024-02-22',
-      events: [
-        {
-          name_subevent: '2ra Tarde Taurina Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-        {
-          name_subevent: '3ra Tarde Taurina Chacas',
-          name_father: 'Fiesta Patronal Chacas',
-          location: 'Chacas',
-          img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
-
-          range_subevent: "18/01 - 25/01",
-        },
-      ]
-    }
-
-  ]
-
-  functAgendaMes() {
-    const data = this.days_month;
+  functAgendaMes(days: any, month_selected: any) {
+    const data = days;
     const result: any[] = [];
 
-    this.monthSelect.forEach((item1) => {
+    month_selected.forEach((item1) => {
       const item2 = data.find((item2: any) => {
         let p1 = moment.utc(item2.fecha);
         let p2 = moment.utc(item1.fecha);
-        // Comparar fechas con Moment.js
         return p1.isSame(p2, 'day');
       });
 
-      if (item2) { // ENCONTRADO
+      if (item2) {
+        // ENCONTRADO
         result.push({
           events: item2.events,
           class: 'A',
           name: item1.name,
           value: item1.value,
           indexWeek: item1.indexWeek,
-          fecha: item1.fecha
+          fecha: item1.fecha,
         });
-      } else { // NO ENCONTRADO
+      } else {
+        // NO ENCONTRADO
         result.push({
           events: [],
           class: '----',
           name: item1.name,
           value: item1.value,
           indexWeek: item1.indexWeek,
-          fecha: item1.fecha
+          fecha: item1.fecha,
         });
       }
     });
 
     this.monthSelect = result;
-    console.log("FINAL: ", this.monthSelect);
-    this.partHorario(this.monthSelect[0])
+
+    //console.log("FINAL: ", this.monthSelect);
   }
 
   viewDaysParts: any[] = []
