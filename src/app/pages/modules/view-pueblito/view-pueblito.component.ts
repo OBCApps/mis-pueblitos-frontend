dbea4581ce8e36aa9f7ea446ea556b3215e5e0cb
wanly13 <wanlyobregon13@gmail.com>
2024-03-14 14:49:01 -0500

    ahora produccion

--- src/app/pages/modules/view-pueblito/view-pueblito.component.ts
+++ src/app/pages/modules/view-pueblito/view-pueblito.component.ts
@@ -187,8 +187,10 @@ export class ViewPueblitoComponent implements OnInit {
           breakpoints: {
             320: { slidesPerView: 2 },
             640: { slidesPerView: 3 },
-            1024: { slidesPerView: 4 },
-            1280: { slidesPerView: 5 },
+            //1024: { slidesPerView: 4 },
+            1024: { slidesPerView: 3 },
+            //1280: { slidesPerView: 5 },
+            1280: { slidesPerView: 3 },
           }
         }
         Object.assign(swiperElemConstructor, swiperOPtions);
