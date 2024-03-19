3f717f3486c669925d13ba25226c6bc12b1cf3e2
wanly13 <wanlyobregon13@gmail.com>
2024-03-19 17:14:56 -0500

    prueba

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true
 };
 
-export const API_SERVER = "http://localhost:3000";
-//export const API_SERVER = 'https://mis-pueblitos-backend.onrender.com'
+//export const API_SERVER = "http://localhost:3000";
+export const API_SERVER = 'https://mis-pueblitos-backend.onrender.com'
 //export const API_SERVER = 'http://ec2-3-90-149-140.compute-1.amazonaws.com'
