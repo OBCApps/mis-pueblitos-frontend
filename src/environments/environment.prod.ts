dc9d09baef464ea2d5bbb4a020909a21c115d261
wanly13 <wanlyobregon13@gmail.com>
2024-04-29 07:05:21 -0500

    dev :')

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ export const environment = {
 };
 
 //export const API_SERVICE_WEB = "http://localhost:3000";
-//export const API_SERVICE_WEB='https://backend.mispueblitos.info';
+export const API_SERVICE_WEB='https://backend.mispueblitos.info';
 //export const API_SERVICE_WEB = 'https://mis-pueblitos-backend.onrender.com'
 //export const API_SERVICE_WEB = 'http://ec2-3-89-144-194.compute-1.amazonaws.com'
 
 
 // Desarrollo 
-export const API_SERVICE_WEB = "http://localhost:3000";
+//export const API_SERVICE_WEB = "http://localhost:3000";
 //export const API_SERVICE_WEB ='https://localhost';
