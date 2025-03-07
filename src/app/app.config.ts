1ba78dbb87c05d3e5483d0b718d1c1ef790189dc
wanly13 <wanlyobregon13@gmail.com>
2025-03-07 02:46:12 -0500

    hash

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,15 +6,17 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
 }
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),    
+    provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withFetch()),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }, // Activar Hash (#)
     importProvidersFrom(
       TranslateModule.forRoot({
         loader: {
