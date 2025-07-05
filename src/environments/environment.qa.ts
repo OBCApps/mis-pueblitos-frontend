63a4e853192788b7830ec1330e5a598d2ba19658
wanly13 <wanlyobregon@gmail.com>
2025-07-05 15:00:25 -0500

    envqa

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false
+};
+
+
+// Producción
+//export const API_SERVICE_WEB = 'https://backend.mispueblitos.pe';
+
+// Pre Producción
+export const API_SERVICE_WEB = 'https://mis-pueblitos-backend.onrender.com'
+
+// Desarrollo
+//export const API_SERVICE_WEB = "http://localhost:3000";
