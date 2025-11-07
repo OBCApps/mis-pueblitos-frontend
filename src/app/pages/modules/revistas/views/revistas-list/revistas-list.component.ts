dce13b1125e6328d4e8678ce7fdd03ac03284ab6
wanly13 <wanlyobregon@gmail.com>
2025-11-07 11:16:43 -0500

    mostrar un pdf terminado

--- src/app/pages/modules/revistas/views/revistas-list/revistas-list.component.ts
+++ src/app/pages/modules/revistas/views/revistas-list/revistas-list.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'app-revistas-list',
   standalone: true,
-  imports: [],
+  imports: [NgxExtendedPdfViewerModule],
   templateUrl: './revistas-list.component.html',
   styleUrl: './revistas-list.component.scss'
 })
 export class RevistasListComponent {
+  // componente.ts
+  pdfPageCount = 5; // queremos mostrar solo hasta la página 5
 
+  afterLoadComplete(pdf: any) {
+    // Esto limita la navegación
+    console.log("pdf",pdf);
+    
+    pdf.numPages = this.pdfPageCount;
+  }
+
+  
 }
